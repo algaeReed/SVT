8101c85c117a2fbc60c1657b2a3148d7d34298f0
张哲 <jefferylong@163.com>
2023-08-30 22:22:59 +0800

    feat: 添加图片

--- src/views/About/Index.tsx
+++ src/views/About/Index.tsx
@@ -1,13 +1,33 @@
 import React from "react";
-
+import { Image } from "antd";
 const SearchResultStyle: React.CSSProperties = {
   color: "black",
+  height: "100%",
+  width: "100%",
+  border: "1px solid red",
 };
+import logo from "../../assets/logo.jpg";
 
+import copyright from "../../assets/copyright.png";
 const About: React.FC = ({}) => {
   return (
     <div style={SearchResultStyle}>
-      <div>About</div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+
+          marginTop: "50%",
+          marginLeft: "50%",
+          transform: "translate(-50%, -50%)",
+        }}
+      >
+        <Image src={logo} width={80} preview={false} />
+
+        <Image src={copyright} width={185} preview={false} />
+      </div>
     </div>
   );
 };
